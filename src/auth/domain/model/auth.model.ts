e187c2a331046c849cba1f5cff8a927f743d480b
cristianaquino <ycristian10@gmail.com>
2024-07-02 15:07:12 -0500

    feat: model for auth entity

--- src/auth/domain/model/auth.model.ts
+++ src/auth/domain/model/auth.model.ts
@@ -1,9 +1,11 @@
+import { User } from '@commons/entities/user.entity';
+
 export class AuthModel {
   id: number;
   code: string;
   email: string;
   activatedAt: string;
-  activated: boolean;
   createdAt: Date;
   updatedAd: Date;
+  user: User;
 }
