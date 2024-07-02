1a1dfa0b2db7f8a469a2c0ebd8e68b105403e6e4
cristianaquino <ycristian10@gmail.com>
2024-07-02 14:58:11 -0500

    feat: dto for in request and out response

---
+++ src/auth/adapter/DTO/In/auth-in.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class RegisterUserDTO {
+  @ApiProperty({
+    required: true,
+    example: 'test@example.com',
+    description: 'User email',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
+
+export class CodeUserDTO {
+  @ApiProperty({
+    required: true,
+    example: 'AXR23Z',
+    description: 'Unique code for the user',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(6)
+  code: string;
+}
