c1d9f2636518cad8c6c70d05a86616621060391c
cristianaquino <ycristian10@gmail.com>
2024-07-02 16:27:24 -0500

    feat: register use case for auth

--- src/auth/usecases/registerUser.usecase.ts
+++ src/auth/usecases/registerUser.usecase.ts
@@ -2,7 +2,6 @@ import { RegisterUserDTO } from '@auth/DTO/In/auth-in.dto';
 import { AuthRepository } from '@auth/repository/auth.interface';
 import { Auth } from '@commons/entities/auth.entity';
 import { IGenerateCodeService } from '@commons/repository/generate_code.interface';
-import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class RegisterUserUseCase {
   constructor(
@@ -11,16 +10,10 @@ export class RegisterUserUseCase {
   ) {}
 
   async execute(registerUserDTO: RegisterUserDTO): Promise<string> {
-    const userExists = await this.authRepository.verifyIfEmailExists(
-      registerUserDTO.email,
-    );
-    if (userExists) {
-      throw new HttpException('Email already exits', HttpStatus.CONFLICT);
-    }
     const auth = new Auth();
     auth.email = registerUserDTO.email;
     auth.code = this.generateCode.generateCode();
-    const response = this.authRepository.register(auth);
-    return response;
+    auth.activatedAt = Date.now().toString();
+    return await this.authRepository.insert(auth);
   }
 }
