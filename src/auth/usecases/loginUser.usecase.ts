f9230d8f80bda9f169d521f04b5e945ee84a6a11
cristianaquino <ycristian10@gmail.com>
2024-07-02 21:19:24 -0500

    feat: use cases for login

--- src/auth/usecases/loginUser.usecase.ts
+++ src/auth/usecases/loginUser.usecase.ts
@@ -1,25 +1,45 @@
-import { RegisterUserDTO } from '@auth/DTO/In/auth-in.dto';
 import { AuthRepository } from '@auth/repository/auth.interface';
+import { IJwtService } from '@commons/adapters/jwt.interface';
+import { JWTConfig } from '@commons/config/jwt.interface';
 import { IGenerateCodeService } from '@commons/repository/generate_code.interface';
-import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class LoginUserUseCase {
   constructor(
-    private authRepository: AuthRepository,
+    private readonly authRepository: AuthRepository,
     private readonly generateCode: IGenerateCodeService,
+    private readonly jwtTokenService: IJwtService,
+    private readonly jwtConfig: JWTConfig,
   ) {}
 
-  async execute(registerUserDTO: RegisterUserDTO): Promise<string> {
-    const userExists = await this.authRepository.verifyIfEmailExists(
-      registerUserDTO.email,
-    );
-
-    if (!userExists) {
-      throw new HttpException('Email no exits', HttpStatus.NOT_FOUND);
-    }
-
+  async execute(email: string): Promise<string> {
+    const userExists = await this.authRepository.findByEmail(email);
     userExists.code = this.generateCode.generateCode();
-    const response = await this.authRepository.login(userExists);
-    return response;
+    userExists.activatedAt = Date.now().toString();
+    return await this.authRepository.insert(userExists);
+  }
+
+  async validateEmailExists(email: string) {
+    const found = await this.authRepository.findByEmail(email);
+    if (!found) return null;
+    return found;
+  }
+
+  async validateCodeExists(code: string) {
+    const codeExists = await this.authRepository.findByCode(code);
+    if (!codeExists) return null;
+    return codeExists;
+  }
+
+  async validateCodeUserAndCreateUserProfile(code: string): Promise<string> {
+    const user = await this.validateCodeExists(code);
+    user.activatedAt = null;
+    user.code = null;
+    const data = await this.authRepository.createProfileUser(user);
+    const token = this.jwtTokenService.createToken(
+      { username: data.name },
+      this.jwtConfig.getJwtSecret(),
+      this.jwtConfig.getJwtExpirationTime(),
+    );
+    return token;
   }
 }
