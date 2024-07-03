56840693d9b09b134475c42ba3f466d5564f7613
cristianaquino <ycristian10@gmail.com>
2024-07-03 17:35:04 -0500

    feat: remove

--- src/users/adapters/DTO/In/user-in.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
-export class CreateUserDTO {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(64)
-  name: string;
-}
